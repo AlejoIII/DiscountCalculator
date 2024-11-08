d652c5a56fcb399548dca22c8a85fd201d8d1cc9
AlejoIII <Loloyum34@gmail.com>
2024-11-08 15:35:32 +0000

    internacionalizar

--- app/src/main/java/com/example/calculator/viewModels/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculator/viewModels/CalculatorViewModel.kt
@@ -1,3 +1,4 @@
+import android.app.AlertDialog
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -7,22 +8,18 @@ class CalculatorViewModel : ViewModel() {
     // Variables de estado para los campos de entrada
     var precio by mutableStateOf("")
     var descuento by mutableStateOf("")
-
     // Variables de estado para los resultados
     var precioFinal by mutableStateOf(0.0)
     var descuentoTotal by mutableStateOf(0.0)
-
-    // Función para actualizar el precio ingresado
+    // Funcion para actualizar el precio ingresado
     fun onPrecioChange(newPrecio: String) {
         precio = newPrecio
     }
-
-    // Función para actualizar el descuento ingresado
+    // Funcion para actualizar el descuento ingresado
     fun onDescuentoChange(newDescuento: String) {
         descuento = newDescuento
     }
-
-    // Función para calcular el descuento y el precio final
+    // Funcion para calcular el descuento y el precio final
     fun calcularDescuento() {
         val precioNum = precio.toDoubleOrNull() ?: 0.0
         val descuentoNum = descuento.toDoubleOrNull() ?: 0.0
@@ -30,8 +27,7 @@ class CalculatorViewModel : ViewModel() {
         descuentoTotal = precioNum * (descuentoNum / 100)
         precioFinal = precioNum - descuentoTotal
     }
-
-    // Función para limpiar los campos
+    // Funcion para limpiar los campos
     fun limpiarCampos() {
         precio = ""
         descuento = ""
